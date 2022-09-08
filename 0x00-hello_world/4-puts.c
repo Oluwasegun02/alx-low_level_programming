7d625b74cf52d5e2c9030b3ebed283e95b86814d
Oluwasegun02 <adebayoogunniyi8@gmail.com>
2022-09-08 14:50:43 -0700

    task 1 to 6 at once

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * puts: prints "\"Programming is like building a multilingual puzzle"
+ *
+ * return: 0
+ */
+
+int main(viod) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * use: printf to print the string of ("%s", "with proper grammar, but the outcome is a piece of art,\n")
+ *
+ * return 0
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
