240880e24a68446e7cfda3a6820a40fabca893b4
Oluwasegun02 <adebayoogunniyi8@gmail.com>
2022-09-08 15:45:34 -0700

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,45 +1,22 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - prints to string
- *
- *   * Description: Prints "and that piece of art is useful.." without puts
- *
- *    * Return: 1
- *
- *     */
-
-
-
+ * main - print string
+ * Description: Prints "and that piece of art is useful.." without puts
+ * Return 1
+ */
 int main(void)
-
 {
-
 	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	long l = 59;
-
-	long fd = 1;
-
+	long 1 = 50;
+        long fd = 1;
 	long syscall = 1;
-
 	long ret = 0;
-
 	__asm__ ("syscall"
-
-			: "=a" (ret)
-
-			: "a" (syscall),
-
-			"D" (fd),
-
-			"S" (s),
-
-			"d" (l));
-
+	: "=a" (ret)
+	: "a" (syscall),
+	"D" (fd),
+	"S" (s),
+	"d" (l));
 	return (1);
-
 }
