90694b701420a7eecad70da21cf0682a31db2866
Oluwasegun02 <adebayoogunniyi8@gmail.com>
2022-09-13 14:33:06 -0700

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
-#include "main.h"
+#include <main.h>
 
 /**
  * main - print putchar
  * Return: always 0
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -17,5 +16,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return (0);
+	return 0;
 }
