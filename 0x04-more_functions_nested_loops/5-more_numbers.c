35dd0c49c5830613c2643cf08f79f0c176794098
Oluwasegun02 <108586588+Oluwasegun02@users.noreply.github.com>
2022-09-16 14:24:31 +0100

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,12 +2,15 @@
 
 /**
  * more_numbers - prints 0 - 14
+ *
  * Return: void
  */
+
 void more_numbers(void)
 {
 	char n, c;
 	int i = 0;
+
 	while (i < 10)
 	{
 		for (n = 0; n <= 14; n++)
@@ -18,8 +21,9 @@ void more_numbers(void)
 				_putchar('1');
 				c = n % 10;
 			}
-			_purchar('0' + c);
+			_putchar('0' + c);
 		}
+
 		_putchar('\n');
 		i++;
 	}
