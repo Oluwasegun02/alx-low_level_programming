03f13e0c7197133b6d7fb85571de51ce8cabc280
Oluwasegun02 <108586588+Oluwasegun02@users.noreply.github.com>
2022-09-30 23:35:46 +0100

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - prints the number of arguments that are passed to it
  * @argc: input
  * @argv: input
  * Return: Always 0 
  */
+ 
 int main(int argc, char *argv[])
 {
 	(void)argv;
