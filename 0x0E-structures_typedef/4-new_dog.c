1cb065c8c2090ea5f0de076f7541bf1add9a9fae
Oluwasegun02 <108586588+Oluwasegun02@users.noreply.github.com>
2022-10-11 00:20:11 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,3 +1,8 @@
+/*
+ * File: 4-new_dog.c
+ * Auth: Gedeon Obae Gekonge
+ */
+
 #include "dog.h"
 #include <stdlib.h>
 
@@ -66,7 +71,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(doggo);
 		return (NULL);
 	}
-doggo->name = _strcopy(doggo->name, name);
+
+	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (doggo->owner == NULL)
+	{
+		free(doggo->name);
+		free(doggo);
+		return (NULL);
+	}
+
+	doggo->name = _strcopy(doggo->name, name);
 	doggo->age = age;
 	doggo->owner = _strcopy(doggo->owner, owner);
 
