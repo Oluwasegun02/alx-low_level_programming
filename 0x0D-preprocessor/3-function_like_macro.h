5d70bd6041089d174d7477054c04aa0528245580
Oluwasegun02 <108586588+Oluwasegun02@users.noreply.github.com>
2022-10-11 01:05:48 +0100

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-
-/*
- * File: 3-function_like_macro.h
- * Auth: Oluwasegun Ogunniyi
- * Desc: Header file that defines a function-like macro ABS(x)
- *       that computes the absolute value of a number x.
- */
-
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-
+#ifndef _FUNC_LIKE_MACRO_H
+#define _FUNC_LIKE_MACRO_H
+#define ABS(x) (((x) < 0) ? -(x) : (x))
 #endif
